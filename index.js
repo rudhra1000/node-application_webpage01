d2069216bb1735df12e7c437281fdb1653b9fe84
rudhra1000 <rudhrasekar100@gmail.com>
2022-04-07 08:46:36 +0530

    nodeapp

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine','ejs');
+
+app.get('/',(req,res) => {
+    res.render('webpage.ejs');
+});
+
+app.listen(5000);
